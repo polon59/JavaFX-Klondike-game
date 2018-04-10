171a8a09274302113ddeb738f602b48863621d22
Mikolaj <mikolaj.rych@gmail.com>
2018-04-10 10:48:30 +0200

    Shuffle deck function

--- src/com/codecool/klondike/Card.java
+++ src/com/codecool/klondike/Card.java
@@ -94,6 +94,7 @@ public class Card extends ImageView {
                 result.add(new Card(suit, rank, true));
             }
         }
+        Collections.shuffle(result);
         return result;
     }
 
