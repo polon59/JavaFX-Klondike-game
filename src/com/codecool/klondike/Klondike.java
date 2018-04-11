f51ffc834d1c7e5352f21e6171fe1a91dcf71eed
polon59 <przemyslaw.polczak@gmail.com>
2018-04-11 12:37:42 +0200

    Implement 'restart game' button with it's functionality in class 'Klondike'

--- src/com/codecool/klondike/Klondike.java
+++ src/com/codecool/klondike/Klondike.java
@@ -7,11 +7,17 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
 
 public class Klondike extends Application {
 
     private static final double WINDOW_WIDTH = 1400;
     private static final double WINDOW_HEIGHT = 900;
+    Button settingsButton;
+    Button newGame;
+    Button undo;
 
     public static void main(String[] args) {
         launch(args);
@@ -22,6 +28,8 @@ public class Klondike extends Application {
         Card.loadCardImages();
         Game game = new Game();
         game.setTableBackground(new Image("/table/green.png"));
+        
+        initializeButtons(game, primaryStage);
         primaryStage.setTitle("Klondike Solitaire");
         primaryStage.setScene(new Scene(game, WINDOW_WIDTH, WINDOW_HEIGHT));
         primaryStage.show();
@@ -38,4 +46,41 @@ public class Klondike extends Application {
         }
     }
 
+    public void initializeButtons(Game game, Stage primaryStage){
+        initializeNewGameButton(game, primaryStage);
+        initializeThemesButton(game);
+        initializeUndoButton(game);
+    }
+    private void initializeNewGameButton(Game game, Stage stage){
+        newGame = new Button("New Game");
+        newGame.setLayoutY(10);
+        newGame.setLayoutX(10);
+        newGame.setOnAction(e -> restartGame(stage));
+        game.getChildren().add(newGame);
+    }
+    public void restartGame(Stage stage){
+        stage.close();
+        start(stage);
+    }
+    private void initializeThemesButton(Game game){
+        settingsButton = new Button("Undo Move");
+        settingsButton.setLayoutY(40);
+        settingsButton.setLayoutX(10);
+        settingsButton.setOnAction(e -> {
+        //SettingsBox.display("Settings", "", game);
+        });
+        game.getChildren().add(settingsButton);
+    }
+    private void initializeUndoButton(Game game){
+        // undo = new Button("Undo");
+        // undo.setLayoutY(70);
+        // undo.setLayoutX(10);
+        // undo.setOnAction(e -> {
+        //     System.out.println("Undo");
+        //     game.getMoves().loadUndoMove();
+        // });
+        // game.getChildren().add(undo);
+    }
 }
+
+
