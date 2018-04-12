c8dd66091ad5fda29845f6d7260f13af8c161a06
Mikolaj <mikolaj.rych@gmail.com>
2018-04-12 16:54:22 +0200

    Move isGameWon condition to class Game

--- src/com/codecool/klondike/Klondike.java
+++ src/com/codecool/klondike/Klondike.java
@@ -11,6 +11,7 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.control.Button;
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
@@ -30,36 +31,13 @@ public class Klondike extends Application {
     @Override
     public void start(Stage primaryStage) {
         Card.loadCardImages();
-        Game game = new Game();
+        Game game = new Game(primaryStage);
         game.setTableBackground(new Image("/table/green.png"));
         
         initializeButtons(game, primaryStage);
         primaryStage.setTitle("Klondike Solitaire");
         primaryStage.setScene(new Scene(game, WINDOW_WIDTH, WINDOW_HEIGHT));
         primaryStage.show();
-
-        // if (game.isGameWon()) {
-        final Stage dialog = new Stage();
-        dialog.initModality(Modality.APPLICATION_MODAL);
-        dialog.initOwner(primaryStage);
-        VBox dialogBox = new VBox(50);
-        dialogBox.getChildren().add(new Text("CONGRATULATIONS! YOU WON!"));
-        Scene dialogScene = new Scene(dialogBox, 300, 200);
-        dialog.setScene(dialogScene);
-        dialogBox.setPadding(new Insets(10, 50, 50, 50));
-        dialogBox.setSpacing(10);
-
-        
-
-        Button start = new Button("START NEW GAME");
-        Button exit = new Button("EXIT");
-
-        start.setOnAction(e -> restartGame(primaryStage));
-        dialogBox.getChildren().add(start);
-        dialogBox.getChildren().add(exit);
-    
-        dialog.show();
-
     }
 
     public void initializeButtons(Game game, Stage primaryStage){
